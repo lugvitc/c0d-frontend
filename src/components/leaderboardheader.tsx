ddd6445a5f4654e837db84d94930d90c1fb117fa
Ananta Krishnan B <anantukid@gmail.com>
2024-09-14 18:31:46 +0530

    fix: made the leaderboard into a table component

--- src/components/leaderboardheader.tsx
+++ src/components/leaderboardheader.tsx
@@ -2,26 +2,25 @@ import { forwardRef } from "react";
 import { cn } from "~/lib/utils";
 
 const LeaderboardHeader = forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...other }, ref) => {
   return (
-    <div
+    <thead
       ref={ref}
       className={cn(
-        "flex items-center justify-between rounded-md bg-[#1D1D1D] p-4 font-orbitron uppercase text-white",
+        "bg-[#1D1D1D] font-orbitron text-sm uppercase tracking-wider text-white",
         className,
       )}
       {...other}
     >
-      <div className="inline-flex w-1/2 items-center space-x-2 font-orbitron">
-        <div className="mr-1">POS.</div>
-        <div>Team Name</div>
-      </div>
-
-      <div className="ml-20 w-1/6 font-orbitron">POINTS</div>
-      <div className="w-1/6 font-orbitron">SOLVED COUNT</div>
-    </div>
+      <tr className="text-left">
+        <th className="w-1/6 px-4 py-3">POS</th>
+        <th className="w-1/2 px-4 py-3">Team Name</th>
+        <th className="w-1/6 px-4 py-3 text-center">Points</th>
+        <th className="w-1/6 px-4 py-3 text-center">Solved Count</th>
+      </tr>
+    </thead>
   );
 });
 
